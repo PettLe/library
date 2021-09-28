ffa6dd2c752d68a50da7f46b5782f6d77fc8370c
PettLe <petewaterz@gmail.com>
2021-09-28 10:23:31 +0300

    troublesome start

---
+++ app.js
@@ -0,0 +1,37 @@
+/* CREATE the god damn basic layout so you can make a form*/
+
+
+/* Create an ARRAY, which takes the books (and their data) as an OBJECT. And create a
+function to take user INPUT and do that. Use HTML FORM in a div.*/
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+
+
+}
+
+/* Create a LOOP that goes through the array and shows every book on the page (or in a cards).
+ Might be easier to add few books manually.*/
+
+
+
+ /* NEW BOOK button, which brings up a FORM, which takes new book: author, title, pages and
+ read status */
+
+
+
+ /* Individual DELETE buttons: "You will need to associate your DOM elements with the actual book 
+ objects in some way. One easy solution is giving them a data-attribute that corresponds to the 
+ index of the library array.*/
+
+
+
+ /* Individual buttons to change READ status: "To facilitate this you will want to create the 
+ function that toggles a book’s read status on your Book prototype instance." */
